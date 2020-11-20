ec9c42670df0e45c016817b547b4fec6a83a4a9c
Alexander Wyss <alexsilvan.wyss@gmail.com>
2020-11-20 12:19:46 +0100

    sonos control

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,31 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {
+  }
 
   @Post('api')
   @HttpCode(200)
   api(@Body('text') text: string): void {
     this.appService.call(text);
+  }
+
+  @Post('add')
+  @HttpCode(200)
+  add(@Body('deviceName') deviceName: string, @Body('groupDeviceName') groupDeviceName: string): void {
+    this.appService.getDeviceByName(deviceName).JoinGroup(groupDeviceName).catch(console.error);
+  }
+
+  @Post('remove')
+  @HttpCode(200)
+  remove(@Body('deviceName') deviceName: string): void {
+    this.appService.leaveGroup(this.appService.getDeviceByName(deviceName));
+  }
+
+  @Get('devices')
+  devices(): string[] {
+    return this.appService.getDevices();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { SonosManager } from '@svrooij/sonos';
+import SonosDevice from '@svrooij/sonos/lib/sonos-device';
 
 @Injectable()
 export class AppService {
+  private manager: SonosManager;
+
+  constructor() {
+    this.manager = new SonosManager();
+    this.manager.InitializeFromDevice('192.168.1.120')
+      .then(console.log)
+      .catch(console.error);
+  }
+
   call(text: string) {
     console.log('api: ' + text);
+  }
+
+  getDevices(): string[] {
+    return this.manager.Devices.map(d => d.Name);
+  }
+
+
+  getDeviceByName(groupDeviceName: string): SonosDevice {
+    return this.manager.Devices.filter(device => device.Name === groupDeviceName)[0];
+  }
+
+  leaveGroup(device: SonosDevice) {
+    device.AVTransportService.BecomeCoordinatorOfStandaloneGroup().catch(console.error);
   }
 }
